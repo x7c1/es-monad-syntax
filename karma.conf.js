5289c9abaffd4b996166ae0adbe2f5a50af0de20
x7c1 <jmtaro+x7c1@gmail.com>
2017-03-23 09:00:10 +0900

    use es5 when testing

--- karma.conf.js
+++ karma.conf.js
@@ -50,6 +50,8 @@ module.exports = function(config) {
       'tests/**/*.spec.js' : ['webpack', 'sourcemap']
     },
 
+    webpack: require('./webpack.config'),
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
